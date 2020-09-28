cf423f2762045bf3a332ee228e0d90e908b1da02
Teodor Levakov <t.levakov@abv.bg>
2020-09-28 23:53:27 +0200

    Make httpClient async

--- MyWebServer/MyHttpClient/Program.cs
+++ MyWebServer/MyHttpClient/Program.cs
@@ -10,10 +10,10 @@ namespace MyHttpClient
 {
     class Program
     {
+        const string NewLine = "\r\n";
         static void Main(string[] args)
         {
             //await ReadData();
-            const string NewLine = "\r\n";
 
             TcpListener tcpListener = new TcpListener(
                 IPAddress.Loopback, 80);
@@ -22,37 +22,42 @@ namespace MyHttpClient
             while (true)
             {
                 var client = tcpListener.AcceptTcpClient();
-                using (var stream = client.GetStream())
-                {
-                    byte[] buffer = new byte[1000000];
-                    var lenght = stream.Read(buffer, 0, buffer.Length);
-
-                    string requestString =
-                        Encoding.UTF8.GetString(buffer, 0, lenght);
-                    Console.WriteLine(requestString);
-
-                    string html = $"<h1>Hello from TeoServer {DateTime.Now}</h1>" +
-                        $"<form method=post><input name=username /><input name=password />" +
-                        $"<input type=submit /></form>";
-
-                    string response = "HTTP/1.1 200 OK" + NewLine +
-                        "Server: TeoServer 2020" + NewLine +
-                        //"Location: https://www.google.com" + NewLine +
-                        //"Content-Disposition: attachment; filename=teo.txt" + NewLine +
-                        "Content-Type: text/html; charset=utf-8" + NewLine +
-                        "Content-Lenght: " + html.Length + NewLine +
-                        NewLine +
-                        html + NewLine;
-
-                    byte[] responseByts = Encoding.UTF8.GetBytes(response);
-                    stream.Write(responseByts);
-
-                    Console.WriteLine(new string('=', 50));
-
-                }
+                ProcesseClientAsync(client);
             }
+
         }
 
+        public static async Task ProcesseClientAsync(TcpClient client) 
+        {
+            using (var stream = client.GetStream())
+            {
+                byte[] buffer = new byte[1000000];
+                var lenght = await stream.ReadAsync(buffer, 0, buffer.Length);
+
+                string requestString =
+                    Encoding.UTF8.GetString(buffer, 0, lenght);
+                Console.WriteLine(requestString);
+
+                string html = $"<h1>Hello from TeoServer {DateTime.Now}</h1>" +
+                    $"<form method=post><input name=username /><input name=password />" +
+                    $"<input type=submit /></form>";
+
+                string response = "HTTP/1.1 200 OK" + NewLine +
+                    "Server: TeoServer 2020" + NewLine +
+                    //"Location: https://www.google.com" + NewLine +
+                    //"Content-Disposition: attachment; filename=teo.txt" + NewLine +
+                    "Content-Type: text/html; charset=utf-8" + NewLine +
+                    "Content-Lenght: " + html.Length + NewLine +
+                    NewLine +
+                    html + NewLine;
+
+                byte[] responseByts = Encoding.UTF8.GetBytes(response);
+                await stream.WriteAsync(responseByts);
+
+                Console.WriteLine(new string('=', 50));
+
+            }
+        }
         public static async Task ReadData()
         {
             Console.OutputEncoding = Encoding.UTF8;
---
+++ MyWebServer/Vic-ove/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Vic_ove
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stopwatch sw = Stopwatch.StartNew();
+
+            List<Task> tasks = new List<Task>();
+
+            for (int i = 1; i <= 50; i++)
+            {
+                var task = Task.Run(async () =>
+                {
+                    HttpClient httpClient = new HttpClient();
+                    var url = $"https://vicove.com/vic-{i}";
+
+                    var responce = await httpClient.GetAsync(url);
+                    var vic = await responce.Content.ReadAsStringAsync();
+
+                    Console.WriteLine(vic.Length);
+                });
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+            
+            Console.WriteLine(sw.Elapsed);
+            Console.WriteLine();
+
+        }
+    }
+}
